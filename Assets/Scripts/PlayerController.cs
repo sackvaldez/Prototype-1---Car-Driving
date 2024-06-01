314cce43c56cf4069e0053e36f256f9123bdc8d6
Sack <rauldaxo@gmail.com>
2024-06-01 14:29:16 -0600

    Movement Input Side to Side and Forward to backward
    Using public variables, now it is possible to move in more directions using input in the translate methods

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 50.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+
+        horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
+        forwardInput = UnityEngine.Input.GetAxis("Vertical");
+
+        // Move the vehicle forward according time and speed variable
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed * forwardInput);
+        // Turn right or left depending on the turnSpeed variable and horizontalInput value
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
