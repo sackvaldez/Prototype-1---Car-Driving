a81e560a9cea81abb93fdc1a07156aa55daba8a2
Sack <rauldaxo@gmail.com>
2024-06-01 09:41:53 -0600

    Camera following player from behind

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float Speed = 50.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        transform.position = player.transform.position + new Vector3(0, 4, -8);
     }
 }
